93fdec4537b91a751974df7a2e2232d9bcf2f0b9
RAFAEL TORICES <torices.rafael@gmail.com>
2024-01-11 08:58:37 +0100

    config telegram test

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 
 from flask import Flask, request, render_template
-# from servicio import Servicio
+from app.servicio import Servicio
 
 app = Flask(__name__, template_folder='app/templates')
 
@@ -11,7 +11,7 @@ def __init__():
     # Compruebo si es un POST
     if request.method == 'POST':
         # Llamo a la clase que inicia el servicio
-        # Servicio(request.json)
+        Servicio(request.json)
         return render_template('home.html')
 
     # Si es una peticion GET le devuelvo la home html
--- src/app/config.py
+++ src/app/config.py
@@ -1,11 +1,11 @@
 # Parametros de configuracion del servicio
 class TelegramConfig():
-    APIURL = ""
-    TOKEN = ""
-    TITULO_APP = ""
-    TELEFONO_SOPORTE = ""
-    CHAT_ID_SOPORTE = ""
-    EMAIL_SOPORTE = ""
+    APIURL = 'https://api.telegram.org/bot'
+    TOKEN = '6772558312:AAFRP4s5tZZQy2L3APUqF6AHIUTU_2sSuwU'
+    TITULO_APP = "DevopsTorBot\n\n"
+    TELEFONO_SOPORTE = "667741333"
+    CHAT_ID_SOPORTE = "409645379"
+    EMAIL_SOPORTE = "\n\nsoporte@rafaeltorices.com\n"
 
 # Configuracion de MySQL
 
